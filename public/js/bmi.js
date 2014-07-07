5fe41c0f5e3f05c7339b205cbd4723636b0c0b8b
Youjin Shin <meggonagul@gmail.com>
2014-07-07 12:21:40 -0400

    edit

--- public/js/bmi.js
+++ public/js/bmi.js
@@ -1,13 +1,13 @@
 var margin = {top: 10, right: 0, bottom: 10, left: 0},
-    width = parseInt(d3.select('#mybmiG').style('width'), 10),
-    width = width - margin.left - margin.right,
-    height = 50 - margin.top - margin.bottom;
+    width2 = parseInt(d3.select('#mybmiG').style('width'), 10),
+    width2 = width2 - margin.left - margin.right,
+    height2 = 50 - margin.top - margin.bottom;
     // width = 220 - margin.left - margin.right,
     // height = 50 - margin.top - margin.bottom;
 
-var svg = d3.select("#mybmiG").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+var svg2 = d3.select("#mybmiG").append("svg")
+    .attr("width", width2 + margin.left + margin.right)
+    .attr("height", height2 + margin.top + margin.bottom)
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
@@ -46,24 +46,24 @@ function showText(t) {
 function drawLine(t) {
   var txScale = d3.scale.linear()
                   .domain([0, maxWidth])
-                  .range([0, width]);
+                  .range([0, width2]);
 
     var tx = txScale( Math.max(0+0.0, Math.min(maxWidth-0.0, t)) );
 
-    var bmiline = svg.append("line")
+    var bmiline = svg2.append("line")
       .attr("x1", tx)
       .attr("x2", tx)
       .attr("y1", 0)
-      .attr("y2", height)
+      .attr("y2", 50)
       .attr("stroke", "#000")
-      .attr("stroke-width", 1)
+      .attr("stroke-width", 2)
       .style("opacity", 1); 
 }
 
 function drawrect() {
   var tempScale = d3.scale.linear()
                   .domain([0, maxWidth])
-                  .range([0, width]);
+                  .range([0, width2]);
 
   var x1 = tempScale(18.5);
   var x2 = tempScale(25);
@@ -74,17 +74,17 @@ function drawrect() {
   var h2 = 0;
   var h = 30;
 
-  var underw = svg.append("rect")
+  var underw = svg2.append("rect")
                   .attr("x", 0)
                   .attr("y", h2+g)
                   .attr("width", x1)
                   .attr("height", h)
-                  .style("fill", "#8dd4ff")
+                  .style("fill", "#b1ddff")//#8dd4ff")
                 // .append("line")
                   .attr("stroke-width", 2)
                   .attr("stroke", "white");
 
-  var normalw = svg.append("rect")
+  var normalw = svg2.append("rect")
                   .attr("x", x1)
                   .attr("y", h2+g)
                   .attr("width", x2-x1)
@@ -93,21 +93,21 @@ function drawrect() {
                   .attr("stroke-width", 2)
                   .attr("stroke", "white");
 
-  var overw = svg.append("rect")
+  var overw = svg2.append("rect")
                   .attr("x", x2)
                   .attr("y", h2+g)
                   .attr("width", x3-x2)
                   .attr("height", h)
-                  .style("fill", "#ffb5b3")
+                  .style("fill", "#ffd3b1")//#0092ff")
                   .attr("stroke-width", 2)
                   .attr("stroke", "white");
 
-  var obesityw = svg.append("rect")
+  var obesityw = svg2.append("rect")
                   .attr("x", x3)
                   .attr("y", h2+g)
                   .attr("width", x4-x3)
                   .attr("height", h)
-                  .style("fill", "#ff0f00")
+                  .style("fill", "#ff6d00")//"#ff0f00")
                   .attr("stroke-width", 2)
                   .attr("stroke", "white");
 }
---
+++ public/js/us_obesity.js
@@ -0,0 +1,82 @@
+var width = 760;
+	height = 400;
+
+var rateById = d3.map();
+var rateByState = d3.map();
+
+// var quantize = d3.scale.quantize()
+// 	// .domain([0, .15])
+// 	.domain([20, 35])
+// 	.range(d3.range(4).map(function(i) { return "q"+i+"-9"}));
+
+function quantize(t) {
+	var bmiStatus;
+	if(t <= 18.5) {
+		bmiStatus = "q0-9";//"Underweight";
+	} else if(t > 18.5 && t <= 25) {
+		bmiStatus = "q1-9";//"Normal status";
+  	} else if(t > 25 && t < 30) {
+    	bmiStatus = "q2-9";//"Overweight";
+  	} else if(t >= 30) {
+    	bmiStatus = "q3-9";//"Obesity";
+  	}	
+  	return bmiStatus;
+}
+
+var projection = d3.geo.albersUsa()
+	.scale(width)
+	.translate([width/2, height/2]);
+
+var path = d3.geo.path()
+	.projection(projection);
+
+var svg = d3.select("body").append("svg")
+	.attr("width", width)
+	.attr("height", height);
+
+var tooltip = d3.select("body")
+	.append("div")
+	.attr("id", "tooltip");
+
+queue()
+	// .defer(d3.json, "us.json") // topojson
+	.defer(d3.json, "us-states.json") // geojson
+	.defer(d3.tsv, "temp.tsv", function(d) {
+		rateById.set(d.id, +d.rate);
+	})
+	.defer(d3.tsv, "obesity_state_2011.tsv", function(d) {
+		// console.log(d);
+		rateByState.set(d.state, +d.rate);
+	})
+	.await(ready);
+
+function ready(error, us) {
+
+	svg.append("g")
+			.attr("class", "states")
+		.selectAll("path")
+			// .data(topojson.feature(us, us.objects.states).features) // topojson
+			.data(us.features) // geojson		
+		.enter().append("path")
+			.attr("class", function(d) {
+				// return quantize(rateById.get(d.id));
+				return quantize(rateByState.get(d.properties.name));
+			})
+			.attr("d", path)
+			// for tooltip
+	    .on("mouseover", function(d){
+	      	// tooltip.text(d.properties.name+": " + rateById.get(d.id) );
+	      	tooltip.text(d.properties.name+": " + rateByState.get(d.properties.name) );  	
+	      	this.style.stroke = "black"; 
+	      	// d3.select(this).style('stroke','#000');
+	      	// d3.select(this).style('stroke-width','2px');
+	      	tooltip.style("visibility", "visible");
+	    })
+	    .on("mousemove", function(){
+	      tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
+	    .on("mouseout", function(){
+	    	this.style.stroke = "white"; 
+	      // d3.select(this).style('stroke-width','1px');
+	      // d3.select(this).style('stroke','#fff');
+	      tooltip.style("visibility", "hidden");});
+}
